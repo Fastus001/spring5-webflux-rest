db89c1e4d59dc436812fee68d14a43711c0b3c4d
Fastus001 <tkarczmarzyk@gmail.com>
2021-03-22 22:08:35 +0100

    417 - Update Category with PUT.

--- src/test/java/pl/fastust/spring5webfluxrest/controllers/CategoryControllerTest.java
+++ src/test/java/pl/fastust/spring5webfluxrest/controllers/CategoryControllerTest.java
@@ -63,4 +63,19 @@ class CategoryControllerTest {
                 .expectStatus()
                 .isCreated();
     }
+
+    @Test
+    void update(){
+        BDDMockito.given(categoryRepository.save(any(Category.class)))
+                .willReturn(Mono.just(Category.builder().description("descrp").build()));
+
+        final Mono<Category> catToUpdateMono = Mono.just(Category.builder().description("Some Cat").build());
+
+        webTestClient.put()
+                .uri("/api/v1/categories/someId")
+                .body(catToUpdateMono, Category.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
+    }
 }
